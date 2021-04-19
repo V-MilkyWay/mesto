634272cdd924821978f0dcf6e599b1f6c28f8778
Vladislav Milushkov <radshura@yandex.ru>
2021-04-19 23:23:56 +0300

    finaly

--- scripts/index.js
+++ scripts/index.js
@@ -2,36 +2,40 @@ let page = document.querySelector('.page');
 let main = document.querySelector('.main');
 let profile = main.querySelector('.profile');
 let profileInfo = profile.querySelector('.profile-info');
-let formElement = page.querySelector('.popup');
+let popup = page.querySelector('.popup');
+let popupContainer = popup.querySelector('.popup__container');
+let formElement = popupContainer.querySelector('.form');
 
 let editButton = profileInfo.querySelector('.profile-info__edit-button');
-let closeButton = formElement.querySelector('.popup__close-button');
-let saveButton = formElement.querySelector('.popup__save-button');
+let closeButton = popupContainer.querySelector('.popup__close-button');
+let saveButton = formElement.querySelector('.form__save-button');
 
 let jobOutput = profileInfo.querySelector('.profile-info__job');
 let nameOutput = profileInfo.querySelector('.profile-info__name');
 
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__job');
+let nameInput = formElement.querySelector('.form__input_name');
+let jobInput = formElement.querySelector('.form__input_job');
 
 function openPopup() {
     nameInput.value = nameOutput.textContent;
     jobInput.value = jobOutput.textContent;
-    formElement.classList.add('popup_opened');
-}
-
-editButton.addEventListener('click', openPopup);
+    console.log('ddd')
+    popup.classList.add('popup_opened');
+};
 
 function closePopup() {
-    formElement.classList.remove('popup_opened');
-}
+    console.log('ddd')
+    popup.classList.remove('popup_opened');
+};
 
-closeButton.addEventListener('click', closePopup);
-
-function savePopup() {
+function formSubmitHandler(e) {
+    e.preventDefault();
+    console.log('sss');
     nameOutput.textContent = nameInput.value;
     jobOutput.textContent = jobInput.value;
-    formElement.classList.remove('popup_opened');
-}
+    popup.classList.add('popup_opened');
+};
 
-saveButton.addEventListener('click', savePopup);
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+saveButton.addEventListener('submit', formSubmitHandler);
