ea711d40dac55370ad7ec61fc176a91b0b9db9c7
Vladislav Milushkov <radshura@yandex.ru>
2021-05-16 21:32:44 +0300

    комментарий к коммиту

--- scripts/index.js
+++ scripts/index.js
@@ -187,7 +187,6 @@ popups.forEach((popup) => {
 // close all popups - overlay
 popups.forEach((popup) => {
     popup.addEventListener('click', function(evt) {
-        console.log(evt.target)
         if (evt.target === popup) {
             closePopup(popup);
             resetForm(formAddCard);
