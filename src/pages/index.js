460743250711d0d36696150b1be7f05b1e73fdd4
Vladislav Milushkov <radshura@yandex.ru>
2021-07-07 22:49:29 +0300

    10/10

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import { PopupWithImage } from '../components/PopupWithImage.js';
 import { PopupWithForm } from '../components/PopupWithForm.js';
 import { PopupWithDeletionButton } from '../components/PopupWithDeletionButton.js';
 import { UserInfo } from '../components/UserInfo.js';
+import { Api } from '../components/Api.js';
 import {
     formEditProfile,
     formAddCard,
@@ -31,7 +32,7 @@ cardAdd.toggleButtonState();
 const popupEditProfile = new PopupWithForm('.popup_type_redact', formEditProfileSubmitHandler);
 const popupAddCard = new PopupWithForm('.popup_type_add-card', submitAddCardForm);
 const popupRedactAvatar = new PopupWithForm('.popup_type_redact-avatar', submitRedactAvatarForm);
-const popupDeletion = new PopupWithDeletionButton('.popup_type_deletion', deleteCardFromServer);
+const popupDeletion = new PopupWithDeletionButton('.popup_type_deletion', deleteServerCard);
 const popupImage = new PopupWithImage('.popup_type_image');
 
 const userInfo = new UserInfo(selectorsAll);
@@ -43,14 +44,12 @@ const addSection = new Section({
     },
     selectorsAll.elements
 );
-//initial card from server
-initCardsFromServer();
 
 function formEditProfileSubmitHandler(evt, data) {
     evt.preventDefault();
     renderLoading(true, popupProfileRedact);
     userInfo.setUserInfo(data);
-    loadingUserInfoOnServer();
+    loadingUserInfo();
     popupEditProfile.closePopup();
 };
 
@@ -61,8 +60,8 @@ function renderCard(item) {
         handleCardClick,
         openPopupDeletion,
         buttonDeleteCard,
-        dislikeCards,
-        likeCards);
+        dislikeCard,
+        likeCard);
     const cardElement = card.generateCard();
     return cardElement;
 }
@@ -81,7 +80,7 @@ function submitRedactAvatarForm(evt, data) {
     evt.preventDefault();
     renderLoading(true, popupAvatarRedact);
     userInfo.setAvatarLink(data);
-    loadingNewAvatarOnServer()
+    loadingAvatar();
     popupRedactAvatar.closePopup();
     cardRedactAvatar.toggleButtonState();
 }
@@ -89,7 +88,7 @@ function submitRedactAvatarForm(evt, data) {
 function submitAddCardForm(evt) {
     evt.preventDefault();
     renderLoading(true, popupAddNewCard);
-    loadingNewCardOnServer();
+    loadingNewCard();
     popupAddCard.closePopup();
     cardEditProfile.toggleButtonState();
     cardAdd.toggleButtonState();
@@ -128,74 +127,44 @@ function handleCardClick(elementImage) {
 }
 
 /////API
-
-function renderError(err) {
-    result.textContent = '';
-    error.textContent = err;
-}
-
-
-//initial users
-fetch('https://nomoreparties.co/v1/cohort-25/users/me', {
-        headers: {
-            authorization: '3f7400de-4faa-456b-995e-bfe48f676c49'
-        }
-    })
-    .then(res => res.json())
-    .then((result) => {
-        document.getElementById('name').textContent = result.name;
-        document.getElementById('about').textContent = result.about;
-        document.getElementById('avatar').src = result.avatar;
-    });
-
+const api = new Api({
+    baseUrl: 'https://nomoreparties.co/v1/cohort-25',
+    headers: {
+        authorization: '3f7400de-4faa-456b-995e-bfe48f676c49',
+        'Content-Type': 'application/json'
+    }
+});
 
 //initial card from server
-function initCardsFromServer() {
-    return fetch('https://nomoreparties.co/v1/cohort-25/cards', {
-            headers: {
-                authorization: '3f7400de-4faa-456b-995e-bfe48f676c49'
-            }
-        })
-        .then(res => res.json())
-        .then(result => {
-            addSection.renderItems(result.reverse());
-        })
-}
-//loading info about user on server
-function loadingUserInfoOnServer() {
-    fetch('https://mesto.nomoreparties.co/v1/cohort-25/users/me', {
-            method: 'PATCH',
-            headers: {
-                authorization: '3f7400de-4faa-456b-995e-bfe48f676c49',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name: document.querySelector(selectorsAll.infoName).textContent,
-                about: document.querySelector(selectorsAll.infoJob).textContent
-            })
-        })
-        .catch((err) => {
-            renderError(`Ошибка: ${err}`);
-        })
-        .finally(() => {
-            renderLoading(false, popupProfileRedact);
-        });
-};
+api.initCardsFromServer().then(result => {
+    addSection.renderItems(result.reverse());
+});
+//initial users
+api.initialUsers();
 
+//delete cards from server
+function deleteServerCard(cardId) {
+    api.deleteCardFromServer(cardId)
+}
+//like cards
+function likeCard(likeId) {
+    api.likeCards(likeId);
+}
+
+//dislike cards
+function dislikeCard(likeId) {
+    api.dislikeCards(likeId);
+}
+//loading new avatar on server
+function loadingAvatar() {
+    api.loadingNewAvatarOnServer(selectorsAll.infoAvatar)
+        .finally(() => {
+            renderLoading(false, popupAvatarRedact);
+        });
+}
 //loading new cards on server 
-function loadingNewCardOnServer() {
-    return fetch('https://mesto.nomoreparties.co/v1/cohort-25/cards', {
-            method: 'POST',
-            headers: {
-                authorization: '3f7400de-4faa-456b-995e-bfe48f676c49',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name: document.querySelector(selectorsAll.infoTitle).value,
-                link: document.querySelector(selectorsAll.infoLink).value,
-            })
-        })
-        .then(res => res.json())
+function loadingNewCard() {
+    api.loadingNewCardOnServer(selectorsAll.infoTitle, selectorsAll.infoLink)
         .then(result => {
             addSection.addItem(renderCard(result));
         })
@@ -206,69 +175,17 @@ function loadingNewCardOnServer() {
             renderLoading(false, popupAddNewCard);
         });
 }
-
-
-//delete cards from server
-function deleteCardFromServer(cardId) {
-    return fetch(`https://mesto.nomoreparties.co/v1/cohort-25/cards/${cardId}`, {
-            method: 'DELETE',
-            headers: {
-                authorization: '3f7400de-4faa-456b-995e-bfe48f676c49',
-            }
-        })
-        .catch((err) => {
-            renderError(`Ошибка: ${err}`);
-        });
-}
-
-//like cards
-function likeCards(likeId) {
-    return fetch(`https://mesto.nomoreparties.co/v1/cohort-25/cards/likes/${likeId}`, {
-            method: 'PUT',
-            headers: {
-                authorization: '3f7400de-4faa-456b-995e-bfe48f676c49',
-            }
-        })
-        .catch((err) => {
-            renderError(`Ошибка: ${err}`);
-        });
-}
-
-//dislike cards
-function dislikeCards(likeId) {
-    return fetch(`https://mesto.nomoreparties.co/v1/cohort-25/cards/likes/${likeId}`, {
-            method: 'DELETE',
-            headers: {
-                authorization: '3f7400de-4faa-456b-995e-bfe48f676c49',
-            }
-        })
-        .catch((err) => {
-            renderError(`Ошибка: ${err}`);
-        });
-}
-
-//loading new avatar on server
-function loadingNewAvatarOnServer() {
-    fetch('https://mesto.nomoreparties.co/v1/cohort-25/users/me/avatar', {
-            method: 'PATCH',
-            headers: {
-                authorization: '3f7400de-4faa-456b-995e-bfe48f676c49',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                avatar: document.querySelector(selectorsAll.infoAvatar).src
-            })
-        })
+//loading info about user on server
+function loadingUserInfo() {
+    api.loadingUserInfoOnServer(selectorsAll.infoName, selectorsAll.infoJob)
         .catch((err) => {
             renderError(`Ошибка: ${err}`);
         })
         .finally(() => {
-            renderLoading(false, popupAvatarRedact);
+            renderLoading(false, popupProfileRedact);
         });
-};
+}
 //good UX
-
-
 function renderLoading(isLoading, popup) {
     if (isLoading) {
         popup.querySelector('.form__loading').classList.add('form__loading_visible');
@@ -278,20 +195,3 @@ function renderLoading(isLoading, popup) {
         popup.querySelector('.form__save-button').classList.remove('form__save-button_hidden');
     }
 }
-
-/*
-если isLoading равен true, 
-элементу spinner нужно добавить класс spinner_visible,
- а элементу content — класс content_hidden
-  (это нужно, чтобы скрыть этот блок пока идёт загрузка);
-  +++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  function renderLoading(isLoading){
-   if (isLoading) {
-    spinner.classList.add('spinner_visible');
-    content.classList.add('content_hidden');
-  } else {
-    spinner.classList.remove('spinner_visible');
-    content.classList.remove('content_hidden');
-  }
-} 
- */
